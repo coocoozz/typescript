4893d190b809e3718a972f5bea46c9a06d306c24
kkwang <coocoozz@naver.com>
2021-06-08 00:49:43 +0900

    add config chapter

--- 9-advanced/9-5-readonly.ts
+++ 9-advanced/9-5-readonly.ts
@@ -3,4 +3,10 @@
     title: string;
     description: string;
   };
+
+  function display(todo: Readonly<ToDo>) {
+    // todo.title = 'dd'; -> read only 타입으로 transform 되었으므로 불변성이 유지된다
+  }
+
+  // Readonly, Pick 등 기존 타입을 transform 할수 있는 사전에 정의된 타입들이 존재한다
 }
